b62dac653ab1b9d443bda497da560e87e2584b49
wangshuaijing <wangshuaijing@58.com>
2021-04-15 14:51:42 +0800

    删除无用的引用

--- eureka-client-b/src/main/java/com/zhqy/springcloud/eureka/client/controller/IndexController.java
+++ eureka-client-b/src/main/java/com/zhqy/springcloud/eureka/client/controller/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
